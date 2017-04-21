31a1cb4f2d3abdf88285e3e5b4b12d068ff2f120
Fr3qu3ntFly3r <bszamody@gmail.com>
2017-04-21 23:19:57 +0200

    Initial commit

---
+++ GitterTest/GitterTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitterTest
+//
+//  Created by Szamódy Zs. Balázs on 2017. 04. 21..
+//  Copyright © 2017. Szamódy Zs. Balázs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
